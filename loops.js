161467112393ca7508b6a1bccd29eda51d32c67b
Elaina <elainaseki@gmail.com>
2018-11-28 23:32:12 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,18 @@
+
+const array = [];
+for(let i = 0;i<25; i++){
+array.push(`I am ${i} strange loop.`);
+}
+
+const n = Math.floor(Math.random()*100);
+while(n>0){
+  console.log('done');
+}
+
+function incrementVariable(){
+    const [array, t] = makeArray();
+    const l = array.length;
+}
+do{
+   incrementVariable();
+}while(array);
